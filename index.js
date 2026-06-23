0451dbeb76cf7e9979e69066d9effb1c5989bf10
Y0U-S3RN4M3 <matthew.xavr@gmail.com>
2026-06-23 18:37:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1207,7 +1207,7 @@
             return;
         };
         gameState.prestiges += 1;
-        gameState.cashCount = 10**90;
+        gameState.cashCount = 10;
         gameState.multiplier = 0;
         gameState.payPercent = 100;
         resetAllItems();
@@ -1244,7 +1244,7 @@
         countdownInterval = null;
         window.cashLoop = null;
     
-        gameState.cashCount = 100000000000000;
+        gameState.cashCount = 10;
         gameState.multiplier = 0;
         gameState.payPercent = 100;
         resetAllItems();
