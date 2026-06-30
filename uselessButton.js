e77ee7b0be687c9aa924ccdd9581deef8567467c
Y0U-S3RN4M3 <matthew.xavr@gmail.com>
2026-06-30 19:35:55 +1000

    Update uselessButton.js

--- uselessButton.js
+++ uselessButton.js
@@ -5,14 +5,22 @@
     if(clicks === null){
         clicks = 0;
     }
-    text.textContent = `You have wasted ${clicks} clicks of your time`
+    text.textContent = `You have wasted ${clicks} clicks of your time`;
+    if(clicks === 1){
+        text.textContent = `You have wasted 1 click of your time`;
+    }
 
 
     if(button){
         button.addEventListener('click', () => {
             clicks++;
             localStorage.setItem("clicks", clicks);
-            text.textContent = `You have wasted ${clicks} clicks of your time`
+            if(clicks === 1){
+                text.textContent = `You have wasted 1 click of your time`;
+            }
+            else{
+                text.textContent = `You have wasted ${clicks} clicks of your time`;
+            }
         })
     }
 })(); 
