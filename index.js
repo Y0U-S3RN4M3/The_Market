3f3877c4bdeee5d3fcef69abce1c94176201b82b
Y0U-S3RN4M3 <matthew.xavr@gmail.com>
2026-07-11 12:59:52 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1197,6 +1197,7 @@
         repairGameState();
         
         gameState.cashCount = Math.floor(gameState.cashCount);
+        gameState.workerAmount = Math.round(gameState.workerAmount);
 
         // USERNAME
 
